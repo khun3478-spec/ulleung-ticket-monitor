4d413c8f249057b0a270942171b075faabe702ab
khun3478-spec <khun3478@gmail.com>
2026-07-14 14:04:47 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,10 @@ import os
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+API_URL = "https://island.theksa.co.kr/booking/selectDepartureList"
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+    "Referer": "https://island.theksa.co.kr/page/booking"
+}
